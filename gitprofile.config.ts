bb6928b8b37bba633aa7bf0fe140244f5b66ed47
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-04-10 19:53:02 +0300

    "Update website URL in CONFIG object"

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -69,7 +69,7 @@ const CONFIG = {
     stackoverflow: '',
     skype: '',
     telegram: 'ZoomishWork',
-    website: 'https://zoomish-portfolio.vercel.app/',
+    website: 'https://zoomish-portfolio.vercel.app',
     phone: '+79937534613',
     email: 'zoomish39@gmail.com',
   },
